93c62a3555658e5cb5f7d5d76bd09d05a6fedd1d
kightn <kight@uindy.edu>
2016-03-29 16:49:42 -0400

    switch

---
+++ switch.cpp
@@ -0,0 +1,36 @@
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+ 
+int main ()
+{
+   // local variable declaration:
+  char grade;
+   cout << "What is the grade?";
+  cin >> grade;
+
+   switch(grade)
+   {
+   case 'A' :
+      cout << "Excellent!" << endl;
+      break;
+   case 'B' :
+   case 'C' :
+      cout << "Well done" << endl;
+      break;
+   case 'D' :
+      cout << "You passed" << endl;
+      break;
+   case 'F' :
+      cout << "Better try again" << endl;
+      break;
+   default :
+      cout << "Invalid grade" << endl;
+   }
+   cout << "Your grade is " << grade << endl;
+ 
+   return 0;
+}
