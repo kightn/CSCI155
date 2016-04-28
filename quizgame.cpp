14e5cef5406137c48bdf77d03bf30245c3cb01d9
kightn <kight@uindy.edu>
2016-04-28 16:32:02 -0400

    update

--- quizgame.cpp
+++ quizgame.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
 using namespace std;
 
 int main()
@@ -10,7 +11,8 @@ int score = 0;
 int k = rand() % 5; int l= rand() % 5; int m= rand() % 5; int n = rand() % 5; int o = rand() % 5;
   int a;
  string qa, aa, qb, ab, qc, ac, qd, ad, qe, ae;
-  
+   ofstream outputFile;
+ 
 
 cout << "Enter Question 1: " << endl;
 getline(cin, qa);
@@ -32,108 +34,123 @@ getline(cin, ad);
 getline(cin, qe);
 cout << "Enter Answer 5: " << endl;
 getline(cin, ae);
-  
+ 
   string  ansarray[] = { aa, ab, ac, ad, ae};
  
-cout << " " << qa << "?" << endl;
-cout << "Is this the answer?" << endl;
-  cout << ansarray[k] << endl;
-cout << " 1 = Yes & 2 = No : " << endl;
+cout << " " << qa << "?" << endl << endl;
+cout << "Is this the answer?" << endl << endl;
+  cout << ansarray[k] << endl << endl;
+cout << " 1 = Yes & 2 = No : " << endl << endl;
 cin >> a;
 
 if( a == 1 && k == 0 ){
 score++;
-  cout << "Correct!" << endl;
+  cout << "Correct!" << endl << endl;
 }
-  
+ 
   else if( a == 2 && k != 0){
-    score++;
-    cout << "Correct" << endl;
+	score++;
+	cout << "Correct" << endl << endl;
   }
 
 else{
-cout << "Incorrect" << endl;
+cout << "Incorrect" << endl << endl;
 }
-  
-  cout << " " << qb << "?" << endl;
-cout << "Is this the answer?" << endl;
-  cout << ansarray[l] << endl;
-cout << " 1 = Yes & 2 = No : " << endl;
+ 
+  cout << " " << qb << "?" << endl << endl;
+cout << "Is this the answer?" << endl << endl;
+  cout << ansarray[l] << endl << endl << endl;
+cout << " 1 = Yes & 2 = No : " << endl << endl;
 cin >> a;
 
 if( a == 1 && l == 1){
 score++;
-  cout << "Correct!" << endl;
+  cout << "Correct!" << endl << endl;
 }
-  
+ 
   else if( a == 2 && l != 1){
-    score++;
-    cout << "Correct" << endl;
+	score++;
+	cout << "Correct" << endl << endl;
   }
 
 else{
-cout << "Incorrect" << endl;
+cout << "Incorrect" << endl << endl;
 }
-  
-  cout << " " << qc << "?" << endl;
-cout << "Is this the answer?" << endl;
-  cout << ansarray[m] << endl;
-cout << " 1 = Yes & 2 = No : " << endl;
+ 
+  cout << " " << qc << "?" << endl << endl;
+cout << "Is this the answer?" << endl << endl;
+  cout << ansarray[m] << endl << endl;
+cout << " 1 = Yes & 2 = No : " << endl << endl;
 cin >> a;
 
 if( a == 1 && m == 2 ){
 score++;
-  cout << "Correct!" << endl;
+  cout << "Correct!" << endl << endl;
 }
-  
+ 
   else if( a == 2 && m != 2){
-    score++;
-    cout << "Correct" << endl;
+	score++;
+	cout << "Correct" << endl << endl;
   }
 
 else{
-cout << "Incorrect" << endl;
+cout << "Incorrect" << endl << endl;
 }
-  cout << " " << qd << "?" << endl;
-cout << "Is this the answer?" << endl;
-  cout << ansarray[n] << endl;
-cout << " 1 = Yes & 2 = No : " << endl;
+  cout << " " << qd << "?" << endl << endl;
+cout << "Is this the answer?" << endl << endl;
+  cout << ansarray[n] << endl << endl;
+cout << " 1 = Yes & 2 = No : " << endl << endl;
 cin >> a;
 
 if( a == 1 && n == 3 ){
 score++;
-  cout << "Correct!" << endl;
+  cout << "Correct!" << endl << endl;
 }
-  
+ 
   else if( a == 2 && n != 3){
-    score++;
-    cout << "Correct" << endl;
+	score++;
+	cout << "Correct" << endl << endl;
   }
 
 else{
-cout << "Incorrect" << endl;
+cout << "Incorrect" << endl << endl;
 }
-  
-  cout << " " << qe << "?" << endl;
-cout << "Is this the answer?" << endl;
-  cout << ansarray[o] << endl;
-cout << " 1 = Yes & 2 = No : " << endl;
+ 
+  cout << " " << qe << "?" << endl << endl;
+cout << "Is this the answer?" << endl << endl;
+  cout << ansarray[o] << endl << endl;
+cout << " 1 = Yes & 2 = No : " << endl << endl;
 cin >> a;
 
 if( a == 1 && o == 4 ){
 score++;
-  cout << "Correct!" << endl;
+  cout << "Correct!" << endl << endl;
 }
-  
+ 
   else if( a == 2 && o != 4){
-    score++;
-    cout << "Correct" << endl;
+	score++;
+	cout << "Correct" << endl << endl;
   }
 
 else{
-cout << "Incorrect" << endl;
+cout << "Incorrect" << endl << endl;
 }
-  cout << "Score: " << score << "/5" << endl;
+  cout << "Score: " << score << "/5" << endl << endl;
+ 
+ 
+ 
+  outputFile.open ("test.txt");
+  outputFile << qa << endl;
+  outputFile << aa << endl << endl;
+  outputFile << qb << endl;
+  outputFile << ab << endl << endl;
+  outputFile << qc << endl;
+  outputFile << ac << endl << endl;
+  outputFile << qd << endl;
+  outputFile << ad << endl << endl;
+  outputFile << qe << endl;
+  outputFile << ae << endl << endl;  
+  outputFile.close();
+ 
 return 0;
 }
-# CSCI155 quizgame
